7d3a56ee01aec927327a76f9fb489b07de37bd51
Michael Redig <mredig@gmail.com>
2023-11-12 23:36:57 -0600

    (bump) package requirements

--- Package.swift
+++ Package.swift
@@ -7,6 +7,8 @@ let package = Package(
     name: "SwiftlyDotEnv",
 	platforms: [
 		.macOS(.v13),
+		.iOS(.v16),
+		.tvOS(.v16),
 	],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
